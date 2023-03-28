6fa7f74bc818cbee23911ed9ca55f3090bba3038
ishnoopy <aldringuasa@gmail.com>
2023-03-28 10:53:05 +0800

    test jenkins x10

--- test/survey.test.js
+++ test/survey.test.js
@@ -4,7 +4,7 @@ const chai                  = require('chai');
 const expect                = chai.expect;
 const SurveyModel           = require('../models/survey.model');
 
-//test comment for jenkins push x7
+//test comment for jenkins push x10
 describe("Survey Model", function(){
 
     it('Should return success message when input captcha matches.', function(){
